11f22fee02190e3f1cdf5c5e450220ea40a8981a
Amir Hossein Saeid Mehr <amir.saiedmehr@gmail.com>
2013-12-08 11:02:21 +0330

    New animation added.

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ $( window ).scroll(function() {
   
   if(x==13){
   	$( "div#headermenu" ).css( "display", "block" ).fadeIn( "slow" );
-  	$( "body" ).css( "overflow-y", "hidden" ).fadeOut( "slow" );
   	$( "div#main" ).css( "display", "none" ).fadeOut( "slow" );
   	$( "body" ).css( "overflow-y", "scroll" ).fadeIn( "slow" );
-  	$( "div#two" ).css( "top", "0%" ).fadeIn( "slow" );
   	$( "div#two" ).css( "opacity", "1" ).fadeIn( "slow" );
+  	$( "div#two" ).css( "top", "0%" );
+  	
   }
 });

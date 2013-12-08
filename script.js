50a3db640b22771a450a040216d479ab4c692436
Amir Hossein Saeid Mehr <amir.saiedmehr@gmail.com>
2013-12-08 09:49:59 +0330

    Add new animation to first page.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+x=0;
 $( window ).scroll(function() {
-  $( "div#main" ).css( "display", "none" ).fadeOut( "slow" );
+  
+  //$( "div#main" ).css( "display", "block" ).fadeOut( "slow" );
+  
+  x+=1;
+  
+  if(x==13){
+  	$( "div#headermenu" ).css( "display", "block" ).fadeIn( "slow" );
+  	$( "body" ).css( "overflow-y", "hidden" ).fadeOut( "slow" );
+  	$( "div#main" ).css( "display", "none" ).fadeOut( "slow" );
+  	$( "body" ).css( "overflow-y", "scroll" ).fadeIn( "slow" );
+  	$( "div#two" ).css( "top", "0%" ).fadeIn( "slow" );
+  	$( "div#two" ).css( "opacity", "1" ).fadeIn( "slow" );
+  }
 });

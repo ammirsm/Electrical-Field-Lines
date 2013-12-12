07d22b4e6b6ea07276b2fb39d36649e86b61b9da
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-12-12 16:45:17 +0330

    Change animation.

--- script.js
+++ script.js
@@ -1,48 +1,77 @@
 $(document).ready(function () {
-    var myHeight;
- 
-    function init() {
-        myHeight = $(window).height();
-    	$("body > div").css("height", myHeight + "px");
-    	$("#container > div").css("height", myHeight + "px");
-    	console.log(myHeight + "px");    
-        myWidth = $(window).width();
-    }
+    // Get the page sizes.
     init();
     $(window).resize(function () {
         init();
     });
-	init();    
-	//$( window ).scroll(function() {
-		//console.log(event.scroll());
-		// var scrollTop = parseInt($(window).scrollTop());
-		// headertop= scrollTop/288; /*The persent of the header top opacity*/
-		// subject= scrollTop/400;
-		// scrollme= scrollTop/600;
-		// footerd= scrollTop/myHeight;
-		// animation= 1-footerd;
-		// $( "footer" ).css( "opacity", 1-footerd);
-		// $( "div#main" ).css( "opacity", 1-footerd);
-		// $( "div#two" ).css( "left", (animation*100)+'%').fadeIn( "slow" );
-		// if(scrollTop>=myHeight){
-			// $( "div#headermenu" ).css( "opacity", footerd ).fadeIn( "slow" );
-		// }
-		// else{
-			// $( "div#headermenu" ).css( "opacity", 0 ).fadeIn( "slow" );
-		// }
-		// /*$("div#two").animate({left:(animation*myWidth)+'px'});*/
-		// /*$( "ul#menu" ).css( "opacity", typedscroll );
-		// $( "div#two" ).css( "opacity", x/100).fadeIn( "slow" );*/
-		// /**/
-// 
-	  	// /*$( "div#headermenu" ).css( "display", "block" ).fadeIn( "slow" );
-	  	// $( "div#main" ).css( "display", "none" ).fadeOut( "slow" );
-	  	// $( "body" ).css( "overflow-y", "scroll" ).fadeIn( "slow" );
-// 	  	
-	  	// $( "div#two" ).css( "top", "0%" );*/
-	  	//});
+    
+    // Change page number when mouse wheel move.
   	$(window).mousewheel(function(event, delta, deltaX, deltaY){
-        console.log(event.delta,event.deltaX,event.deltaY);
+    	i = getPageNumber();
+    	
+    	// Select the page number.
+        if(deltaX<0){
+        	page (i+1);
+        }
+        else if (deltaX>0)
+        {
+        	page (i-1);
+        }
+        
         return false;
-    })
+   });  
 });
+
+var myHeight;
+function init() {
+    // Get height and width.
+    myHeight = $(window).height();    
+    myWidth = $(window).width();
+    
+    // Set height on the page.
+	$("body > div").css("height", myHeight + "px");
+	$("#container > div").css("height", myHeight + "px");
+	
+}
+
+var isPageChanging = false;
+function page(pageNumber){
+	// To prevent changing the page during the animation.
+	if(isPageChanging)
+		return;
+	
+	// Limit page number.
+	if(pageNumber < 0 || pageNumber >= $("#container > div").length)
+		return;
+	
+	// Lock changing the page.
+	isPageChanging = true;
+	
+	$("#container").animate({"top": -1 * pageNumber * myHeight + "px"}, 300, "swing", function () {isPageChanging = false; onPageChangeEnd()});
+	onPageChangeStart(pageNumber);
+}
+
+function onPageChangeStart(pageNumber)
+{
+    if(pageNumber > 0){
+    	$("#headermenu").animate({"opacity": "1"});
+    }
+    else{
+    	$("#headermenu").animate({"opacity": "0"});
+    }
+}
+
+function onPageChangeEnd(pageNumber)
+{
+	
+}
+
+function getPageNumber()
+{
+	// Get the page number.
+	var i;
+	b= parseInt($("#container").css("top"));
+	i= Math.round((-1 * b/ myHeight));
+	
+	return i;
+}

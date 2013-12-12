2660208d113b9753a6e50149ee3a1418be779a02
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-12-12 13:11:25 +0330

    change all CSS.:

--- script.js
+++ script.js
@@ -3,40 +3,46 @@ $(document).ready(function () {
  
     function init() {
         myHeight = $(window).height();
-        
+    	$("body > div").css("height", myHeight + "px");
+    	$("#container > div").css("height", myHeight + "px");
+    	console.log(myHeight + "px");    
         myWidth = $(window).width();
     }
     init();
     $(window).resize(function () {
         init();
     });
-	    
-	x=0;
-	$( window ).scroll(function() {
-		var scrollTop = parseInt($(window).scrollTop());
-		headertop= scrollTop/288; /*The persent of the header top opacity*/
-		subject= scrollTop/400;
-		scrollme= scrollTop/600;
-		footerd= scrollTop/myHeight;
-		animation= 1-footerd;
-		$( "footer" ).css( "opacity", 1-footerd);
-		$( "div#main" ).css( "opacity", 1-footerd);
-		$( "div#two" ).css( "left", (animation*100)+'%').fadeIn( "slow" );
-		if(scrollTop>=myHeight){
-			$( "div#headermenu" ).css( "opacity", footerd ).fadeIn( "slow" );
-		}
-		else{
-			$( "div#headermenu" ).css( "opacity", 0 ).fadeIn( "slow" );
-		}
-		/*$("div#two").animate({left:(animation*myWidth)+'px'});*/
-		/*$( "ul#menu" ).css( "opacity", typedscroll );
-		$( "div#two" ).css( "opacity", x/100).fadeIn( "slow" );*/
-		/**/
-
-	  	/*$( "div#headermenu" ).css( "display", "block" ).fadeIn( "slow" );
-	  	$( "div#main" ).css( "display", "none" ).fadeOut( "slow" );
-	  	$( "body" ).css( "overflow-y", "scroll" ).fadeIn( "slow" );
-	  	
-	  	$( "div#two" ).css( "top", "0%" );*/
-	  	});
+	init();    
+	//$( window ).scroll(function() {
+		//console.log(event.scroll());
+		// var scrollTop = parseInt($(window).scrollTop());
+		// headertop= scrollTop/288; /*The persent of the header top opacity*/
+		// subject= scrollTop/400;
+		// scrollme= scrollTop/600;
+		// footerd= scrollTop/myHeight;
+		// animation= 1-footerd;
+		// $( "footer" ).css( "opacity", 1-footerd);
+		// $( "div#main" ).css( "opacity", 1-footerd);
+		// $( "div#two" ).css( "left", (animation*100)+'%').fadeIn( "slow" );
+		// if(scrollTop>=myHeight){
+			// $( "div#headermenu" ).css( "opacity", footerd ).fadeIn( "slow" );
+		// }
+		// else{
+			// $( "div#headermenu" ).css( "opacity", 0 ).fadeIn( "slow" );
+		// }
+		// /*$("div#two").animate({left:(animation*myWidth)+'px'});*/
+		// /*$( "ul#menu" ).css( "opacity", typedscroll );
+		// $( "div#two" ).css( "opacity", x/100).fadeIn( "slow" );*/
+		// /**/
+// 
+	  	// /*$( "div#headermenu" ).css( "display", "block" ).fadeIn( "slow" );
+	  	// $( "div#main" ).css( "display", "none" ).fadeOut( "slow" );
+	  	// $( "body" ).css( "overflow-y", "scroll" ).fadeIn( "slow" );
+// 	  	
+	  	// $( "div#two" ).css( "top", "0%" );*/
+	  	//});
+  	$(window).mousewheel(function(event, delta, deltaX, deltaY){
+        console.log(event.delta,event.deltaX,event.deltaY);
+        return false;
+    })
 });

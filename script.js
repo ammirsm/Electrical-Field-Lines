e582dedf3af0d57c775dc43e1175bf30da3df3ca
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-12-12 20:30:08 +0330

    Fix all the animations. +somemessy codes.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
+jQuery(document).ready(function () {
+	    
     // Get the page sizes.
     init();
-    $(window).resize(function () {
+    jQuery(window).resize(function () {
         init();
     });
     
     // Change page number when mouse wheel move.
-  	$(window).mousewheel(function(event, delta, deltaX, deltaY){
+  	jQuery(window).mousewheel(function(event, delta, deltaX, deltaY){
     	i = getPageNumber();
-    	
     	// Select the page number.
         if(deltaX<0){
         	page (i+1);
@@ -17,20 +17,40 @@ $(document).ready(function () {
         {
         	page (i-1);
         }
-        
         return false;
-   });  
+    });
+	
+	// Change page number when some keys are pressed.
+	jQuery(window).keypress(function( event ) {
+		switch(event.keyCode){
+			case 40: // ArrowUp
+			case 34: // PageUp
+				page (getPageNumber()+1);
+				break;
+			case 38: // ArrowDown
+			case 33: // PageDown
+				page (getPageNumber()-1);
+				break;
+			case 35: //End
+				page (getNumberOfPages()-1);
+				break;
+			case 36: //Home
+				page (0);
+				break; 
+		}
+	});
+	
 });
 
 var myHeight;
 function init() {
     // Get height and width.
-    myHeight = $(window).height();    
-    myWidth = $(window).width();
+    myHeight = jQuery(window).height();    
+    myWidth = jQuery(window).width();
     
     // Set height on the page.
-	$("body > div").css("height", myHeight + "px");
-	$("#container > div").css("height", myHeight + "px");
+	jQuery("body > div").css("height", myHeight + "px");
+	jQuery("#container > div").css("height", myHeight + "px");
 	
 }
 
@@ -41,23 +61,23 @@ function page(pageNumber){
 		return;
 	
 	// Limit page number.
-	if(pageNumber < 0 || pageNumber >= $("#container > div").length)
+	if(pageNumber < 0 || pageNumber >= getNumberOfPages())
 		return;
 	
 	// Lock changing the page.
 	isPageChanging = true;
 	
-	$("#container").animate({"top": -1 * pageNumber * myHeight + "px"}, 300, "swing", function () {isPageChanging = false; onPageChangeEnd()});
+	jQuery("#container").animate({"top": -1 * pageNumber * myHeight + "px"}, 300, "swing", function () {isPageChanging = false; onPageChangeEnd()});
 	onPageChangeStart(pageNumber);
 }
 
 function onPageChangeStart(pageNumber)
 {
     if(pageNumber > 0){
-    	$("#headermenu").animate({"opacity": "1"});
+    	jQuery("#headermenu").animate({"opacity": "1"});
     }
     else{
-    	$("#headermenu").animate({"opacity": "0"});
+    	jQuery("#headermenu").animate({"opacity": "0"});
     }
 }
 
@@ -70,8 +90,28 @@ function getPageNumber()
 {
 	// Get the page number.
 	var i;
-	b= parseInt($("#container").css("top"));
+	b= parseInt(jQuery("#container").css("top"));
 	i= Math.round((-1 * b/ myHeight));
 	
 	return i;
 }
+
+function getNumberOfPages(){
+	// How many page are in our page.
+	return jQuery("#container > div").length;
+}
+
+// var pagination;
+// jQuery(document).ready(function () {
+	// pagination = new Pagination();
+	// pagination.onPageChangeStart = function(pageNumber)
+	// {
+	    // if(pageNumber > 0){
+	    	// jQuery("#headermenu").animate({"opacity": "1"});
+	    // }
+	    // else{
+	    	// jQuery("#headermenu").animate({"opacity": "0"});
+	    // }
+	// };
+	// pagination.apply();
+// });
